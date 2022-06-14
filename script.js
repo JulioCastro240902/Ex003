2f5582d5d6efabf6bbafd90a81d990bc5f66367f
Juliocastro240902 <Juliocastro.contato@gmail.com>
2022-06-14 01:40:20 -0300

    Exercise about loops

---
+++ script.js
@@ -0,0 +1,25 @@
+let count = 1;
+
+for (count; count <= 10; count++) {
+  console.log(count);
+}
+
+let dado = 10;
+
+while (dado >= 1) {
+  console.log(dado);
+  dado--;
+}
+
+let loop = 1;
+
+for (loop; loop <= 100; loop += 2) {
+  console.log(loop);
+}
+
+let hundred = 0;
+
+while (hundred <= 100) {
+  console.log(hundred);
+  hundred += 2;
+}
